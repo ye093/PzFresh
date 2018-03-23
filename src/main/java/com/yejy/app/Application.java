9b5aca3123f0559d9f1b82f96000dc82f83eb1b5
ye093 <ye_093@163.com>
2018-03-23 17:07:15 +0800

    完善Mybatis配置,添加调试日志

--- src/main/java/com/yejy/app/Application.java
+++ src/main/java/com/yejy/app/Application.java
@@ -3,9 +3,11 @@ package com.yejy.app;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.yejy.app.data")
+@ComponentScan
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
